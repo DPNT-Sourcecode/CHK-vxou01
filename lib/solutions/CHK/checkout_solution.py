4be1db01790b84beef899a5e57bdc417166fdba0
sbx_user1051 <sbx_user1051@169.254.32.79>
2025-03-24 23:02:14 +0000

    Mon Mar 24 23:02:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,23 +4,23 @@ from collections import Counter
 # skus = unicode string
 def checkout(skus):
     pricing = {"A": 50, "B": 30, "C": 20, "D": 15} # keeps track of individual sku pricing
-    # skus = skus.split("") # split input into individual sku
+    skus_list = list(skus) # convert string to list
     total = 0 
     
-    for sku in skus:
+    for sku in skus_list:
         if sku not in pricing: # case: illegal input
             return -1
         else:
             total += pricing[sku]
     
     # subtract 'discount'
-    A_count = skus.count("A")
+    A_count = skus_list.count("A")
     total -= (((A_count//3)%3) * 20)
 
-    B_count = skus.count("B")
+    B_count = skus_list.count("B")
     total -= (((B_count//2)%2) * 15)
 
-    return total if sku else -1# returns total amount
+    return total if skus_list else -1 # returns total amount unless empy
 
 
 
