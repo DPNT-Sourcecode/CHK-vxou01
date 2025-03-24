7188e41bbe79845c88e065491b8297bfc3383a19
sbx_user1051 <sbx_user1051@169.254.32.79>
2025-03-24 22:20:04 +0000

    Mon Mar 24 22:20:04 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,11 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    pricing = {"A": 50, "B": 30, "C": 20, "D": 15} 
+    skus = skus.split(",")
+    total = 0
+    for sku in skus:
+        total += pricing[sku]
+    return total
+
+
