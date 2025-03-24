4154524ec8afd90ee57fed50734d45f58a9755b7
sbx_user1051 <sbx_user1051@169.254.32.79>
2025-03-24 22:23:04 +0000

    Mon Mar 24 22:23:04 UTC 2025

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,10 +1,11 @@
-from solutions.CHK import checkout
+from solutions.CHK import checkout_solution
 import pytest
 
 
 class TestCHK():
     def test_chk(self):
-        assert sum_solution.compute(1, 2) == 3
+        assert checkout_solution.checkout("A,B,C,D") == 115
+        assert checkout_solution.checkout("A,A,A") == 130
 
     # def test_sum_error(self):
     #     with pytest.raises(ValueError, match="Input is out of range"):
@@ -12,3 +13,4 @@ class TestCHK():
         
     #     with pytest.raises(ValueError, match="Input is out of range"):
     #         sum_solution.compute(101,2)
+
