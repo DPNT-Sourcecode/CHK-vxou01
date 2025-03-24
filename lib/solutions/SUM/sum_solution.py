b1e7f4f6c32ef19bce16a69ce3a2138c17533326
sbx_user1051 <sbx_user1051@169.254.53.85>
2025-03-24 22:03:20 +0000

    Mon Mar 24 22:03:20 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,6 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    return x+y
-
+    if (0 <= x <= 100) and (0 <= y <= 100):
+        return x+y
+    else:
+        raise ValueError("Input is out of range")
